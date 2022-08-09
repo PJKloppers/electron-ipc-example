e2f75f671f54721a28603ed311e7143a941acaff
Paul Kloppers <paul.kloppers@outlook.com>
2022-08-09 22:35:07 +0200

    added Ipc

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow, Menu, ipcMain} = require('electron');
+const path = require('path');
+const url = require('url');
+
+
+
+let mainWindow;
+app.on('ready', function(){
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+          }
+    });
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, '../frontend/main.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    mainWindow.on('closed', function(){
+        app.quit();
+    } );
+
+
+});
+
+ipcMain.on('data', function(event, data){
+    console.log(data);
+    mainWindow.webContents.send('respond_data', data);
+});
